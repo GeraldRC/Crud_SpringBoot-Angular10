7e8dc6739c1439229bca648ad39df4c003a16ef0
GeraldRC <gerald.reyes@outlook.cl>
2020-10-09 23:12:05 -0300

    first commit

---
+++ springbootbackend/src/main/java/com/crud/springbootbackend/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.crud.springbootbackend.repository;
+
+import com.crud.springbootbackend.model.Employee;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+    
+}
